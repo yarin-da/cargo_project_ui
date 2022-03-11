abb5f89c7d61448b1c9aa14a46882af5d16499dc
HadasBabayov <you@example.com>
2022-03-11 10:20:20 +0200

    add Tooltip

--- src/components/ExampleInputFileButton.js
+++ src/components/ExampleInputFileButton.js
@@ -1,6 +1,7 @@
 import Button from "@mui/material/Button";
 import { saveAs } from "file-saver";
 import logo from './images/logo.png'
+import {Tooltip} from "@mui/material";
 
 function downloadFile(){
     saveAs(
@@ -9,25 +10,25 @@ function downloadFile(){
     );
 }
 
-
 const ExampleInputFileButton = () => {
     return (
-
-        <Button
-            className="upload-button"
-            onClick={downloadFile}
-            component="span"
-            multiple type="file"
-            style={{
-                textTransform: 'none',
-                color: "white",
-                borderColor: "white",
-                marginTop: 25,
-                marginLeft: 40
-            }}
-            variant="outlined">
-            Download Example File
-        </Button>
+        <Tooltip title="Click here if you want to see the file format">
+            <Button
+                className="upload-button"
+                onClick={downloadFile}
+                component="span"
+                multiple type="file"
+                style={{
+                    textTransform: 'none',
+                    color: "white",
+                    borderColor: "white",
+                    marginTop: 25,
+                    marginLeft: 40
+                }}
+                variant="outlined">
+                Download Example File
+            </Button>
+        </Tooltip>
 
     )
 };
