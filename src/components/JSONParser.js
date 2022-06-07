7d652439053918a15e1c0bdbcb12a41dde164d62
HadasBabayov <you@example.com>
2022-06-07 18:51:15 +0300

    add json-input

---
+++ src/components/JSONParser.js
@@ -0,0 +1,147 @@
+import { parseValue, types, stringTypeTesters } from "./Type";
+import Package from "./Package";
+import { t } from "i18next";
+
+const headers = "width,height,depth,cost,maxWeight,type,amount,priority,weight,profit,canRotate,canStackAbove".split(',');
+const cargoHeaders = "width,height,depth,cost,maxWeight".split(',');
+const packageHeaders = "width,height,depth,type,amount,priority,weight,profit,canRotate,canStackAbove".split(',');
+
+function splitRowsToArrays(rows) {
+    for (let i = 0; i < rows.length; i++) {
+        rows[i] = rows[i].split(',');
+    }
+    return rows
+}
+
+function headerToIndexMap(firstRow) {
+    const map = new Map();
+    for (let i = 0; i < firstRow.length; i++) {
+        if (headers.includes(firstRow[i])) {
+            if (!map.has(firstRow[i])) {
+                map.set(firstRow[i], i + 1);
+            } else {
+                return { error: t('duplicateColumn', { column: firstRow[i] }) };
+            }
+        } else {
+            return { error: t('invalidColumn', { column: firstRow[i] }) };
+        }
+    }
+    return { map };
+}
+
+function containerFields(containerRow, map) {
+    const container = {};
+    for (const [col, index] of map) {
+        if (cargoHeaders.includes(col)) {
+            const type = types[col];
+            const tester = stringTypeTesters[type];
+            const value = containerRow[index]
+            if (tester(value)) {
+                container[col] = parseValue(col, value);
+            } else {
+                return { error: t('inputError', { object: t('container'), key: t(col), type: t(type), value }) }
+            }
+        }
+    }
+    return { container };
+}
+
+function packagesFields(packagesRows, map) {
+    const packages = [];
+    for (let i = 0; i < packagesRows.length; i++) {
+        const p = new Package();
+        for (const [col, index] of map) {
+            if (packageHeaders.includes(col)) {
+                const type = types[col];
+                const tester = stringTypeTesters[type];
+                const value = packagesRows[i][index];
+                if (tester(value)) {
+                    p[col] = parseValue(col, value);
+                } else {
+                    return { error: t('inputError', { object: t('package'), key: t(col), type: t(type), value }) }
+                }
+            }
+        }
+        packages.push(p);
+    }
+    return { packages };
+}
+
+function parseJSONFile(file) {
+    // split file text to rows (take into account possible carriage return in windows)
+    let rows = file.split(/\r?\n/);
+    // delete titles
+    let firstRow = rows.shift();
+    let dataRows = splitRowsToArrays(rows);
+
+    let newFirstRow = firstRow.split(',');
+    newFirstRow.shift();
+    let { map: headerToIndex, error } = headerToIndexMap(newFirstRow);
+    if (error) return { error };
+
+    let seenContainer = false;
+    let containerRow = [];
+    let packagesRows = [];
+    for (let i = 0; i < dataRows.length; i++) {
+        if (dataRows[i][0] === 'container' && seenContainer) {
+            containerRow.length = 0;
+            packagesRows.length = 0;
+            return { error: t('mustBeOnlyOneContainer') };
+        }
+        if (dataRows[i][0] === 'container') {
+            seenContainer = true;
+            containerRow = dataRows[i];
+        }
+        if (dataRows[i][0] === 'package') {
+            packagesRows.push(dataRows[i]);
+        }
+    }
+
+    const containerRet = containerFields(containerRow, headerToIndex);
+    if (containerRet.error) return {...containerRet};
+    const packagesRet = packagesFields(packagesRows, headerToIndex);
+    if (packagesRet.error) return {...packagesRet};
+
+    const { packages } = packagesRet;
+    const { container } = containerRet;
+    return { container, packages }
+}
+
+function parseJSON(file, handler) {
+    let reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+        const parsedData = parseJSONFile(reader.result);
+        if (parsedData.error) throw parsedData.error;
+        handler(parsedData);
+    }
+};
+
+function parseArrayToCSV(array) {
+    let ret = '';
+    if (array.length > 0) {
+        const keys = Object.keys(array[0]);
+        ret += keys.join(',') + '\n';
+        array.forEach(element => {
+            ret += keys.map(key => element[key]).join(',') + '\n';
+        });
+    }
+    return ret;
+}
+
+function parseJSONtoCSV(json) {
+    // is 'json' an array?
+    if (!!json && json.constructor === Array) return parseArrayToCSV(json);
+    // is 'json' an object?
+    if (json === Object(json)) return Object.keys(json).map(prop => `\n${prop}\n` + parseJSONtoCSV(json[prop])).join('\n');
+    // 'json' is primitive
+    return json;
+};
+
+export {
+    parseJSON,
+    parseJSONtoCSV,
+    parseJSONFile,
+}
+
+export default parseJSON
