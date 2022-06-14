a7e5091eda504c0bcc2e2edc4aedd3a1c212e74b
HadasBabayov <you@example.com>
2022-06-14 21:53:08 +0300

    add help modal

--- src/components/CustomAppBar.js
+++ src/components/CustomAppBar.js
@@ -1,24 +1,37 @@
-import { useState } from "react";
-import { Button, AppBar, Toolbar, Box, Modal } from "@mui/material";
+import {useState} from "react";
+import {Button, AppBar, Toolbar, Box, Modal} from "@mui/material";
 import SettingsIcon from '@mui/icons-material/Settings';
+import InfoSharpIcon from "@mui/icons-material/InfoSharp";
 import SettingsPage from './SettingsPage';
 import Logo from '../images/logo.png';
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import CustomText from "./CustomText";
+import ShowInfo from "./ShowInfo";
 
-const SettingsButton = ({ onClick }) => {
+const SettingsButton = ({onClick}) => {
     return (
         <Button onClick={onClick}>
-            <SettingsIcon htmlColor="black" fontSize={"medium"} />
+            <SettingsIcon htmlColor="black" fontSize={"medium"}/>
         </Button>
     );
 };
 
-const CustomAppBar = ({ units, setUnits }) => {
+const Information = ({onClick}) => {
+    return (
+        <Button onClick={onClick}>
+            <InfoSharpIcon htmlColor="black" fontSize={"medium"}/>
+        </Button>
+    );
+};
+
+const CustomAppBar = ({units, setUnits}) => {
     const [showSettings, setShowSettings] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
     const toggleSettingsModal = () => setShowSettings(curr => !curr);
+    const toggleInformationModal = () => setShowInfo(curr => !curr);
     const navigate = useNavigate();
     return (
-        <div style={{ boxShadow: '10px 3px 10px rgba(0,0,0,0.25)', zIndex: 1 }}>
+        <div style={{boxShadow: '10px 3px 10px rgba(0,0,0,0.25)', zIndex: 1}}>
             <AppBar position="static" style={{backgroundColor: '#42A5F5'}}>
                 <Toolbar style={{display: 'flex', justifyContent: 'space-between'}}>
                     <Box
@@ -33,18 +46,30 @@ const CustomAppBar = ({ units, setUnits }) => {
                         src={Logo}
                     />
                     <Toolbar>
-                        <SettingsButton onClick={toggleSettingsModal} />
+                        <SettingsButton onClick={toggleSettingsModal}/>
+                    </Toolbar>
+                    <Toolbar>
+                        <Information onClick={toggleInformationModal}/>
                     </Toolbar>
                 </Toolbar>
             </AppBar>
-            <Modal 
-                style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-                open={showSettings} 
+            <Modal
+                style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+                open={showSettings}
                 onClose={toggleSettingsModal}
             >
                 <div className={'settings-modal'}>
-                    <SettingsPage units={units} setUnits={setUnits} />
+                    <SettingsPage units={units} setUnits={setUnits}/>
                 </div>
+            </Modal>
+            <Modal
+                style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+                open={showInfo}
+                onClose={toggleInformationModal}
+            >
+               <div>
+                   <ShowInfo/>
+               </div>
             </Modal>
         </div>
     );
--- src/components/ShowInfo.js
+++ src/components/ShowInfo.js
@@ -1,23 +1,41 @@
 import {Icon, Tooltip} from "@mui/material";
 import InfoSharpIcon from "@mui/icons-material/InfoSharp";
+import CustomText from "./CustomText";
 
-const iconStyle = {
-    marginLeft: 20,
+const style = {
+    width: 500,
+    border: "1 solid black",
+    borderRadius: 15,
+    backgroundColor: "white",
+    padding: 20
 }
 
 const ShowInfo = () => {
     return (
-        <Tooltip title={
-            <div>
-                You can set up only one container.<br/>
-                For fields that are not relevant to the container and packages,
-                check '-' (as in the sample file).
-            </div>
-        }>
-            <Icon style={iconStyle}>
-                <InfoSharpIcon/>
-            </Icon>
-        </Tooltip>
+        <div style={style}>
+            <CustomText text={"Help"} style={{
+                fontSize: 30,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}/>
+            <CustomText text={"In home page:" +
+            "- If you want to enter input, click on Find packing." +
+            "- If you want to insert a solution file to go directly to the 3D screen, click View Solution." +
+            "In 3D page:" +
+            "- If you want to change the color of the packages (by type) click on the color circle in the map on the left" +
+            "- To change the resulting solution, click the edit button and then the package you want to edit. " +
+            "The buttons on the right menu are the same colors as the arrows next to the container - " +
+            "this indicates the direction of movement according to the color of the button." +
+            "- If you want to export the solution, click the download button.\n" +
+            "You can insert the downloaded file as input on the home page after clicking on View solution and immediately switch to the 3D screen."}
+                        style={{
+                            fontSize: 20,
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}/>
+        </div>
     )
 };
 
