231fe491db4acd47f48fa4be2eb370ae509d3fc3
HadasBabayov <you@example.com>
2022-06-15 15:21:39 +0300

    change help information

--- src/components/ShowInfo.js
+++ src/components/ShowInfo.js
@@ -4,6 +4,9 @@ import { useLocation } from "react-router-dom";
 import {Icon, Tooltip} from "@mui/material";
 import InfoSharpIcon from "@mui/icons-material/InfoSharp";
 import CustomText from "./CustomText";
+import findPacking from '../images/findPacking.png'
+import viewSolution from '../images/viewSolution.png'
+import editColors from '../images/typeColor.png'
 
 const style = {
     width: 500,
@@ -18,8 +21,15 @@ const style = {
 // TODO: Add keypress combination images (CTRL+LEFTCLICK, ALT+LEFTCLICK)
 const ViewHelp = () => {
     return (
-        <div>
+        <div style={{display: "flex", flexDirection: "column"}}>
             <CustomText text="If you want to change the color of the packages (by type) click on the color circle in the map on the left." />
+            <img src={editColors} alt={editColors} style={{
+                display: "flex",
+                alignItems: "center",
+                alignSelf: "center",
+                height: 100,
+                width: 160
+            }}/> <br/>
             <CustomText text="To change the resulting solution, click the edit button and then the package you want to edit." />
             <CustomText text="The buttons on the right menu are the same colors as the arrows next to the container - this indicates the direction of movement according to the color of the button." />
             <CustomText text="If you want to export the solution, click the download button." />
@@ -30,9 +40,21 @@ const ViewHelp = () => {
 
 const HomeHelp = () => {
     return (
-        <div>
+        <div style={{display: "flex", flexDirection: "column"}}>
             <CustomText text="If you want to enter input, click on Find packing." />
+            <img src={findPacking} alt={findPacking} style={{
+                display: "flex",
+                alignItems: "center",
+                alignSelf: "center",
+                height: 60,
+                width: 150}}/><br/>
             <CustomText text="If you want to insert a solution file to go directly to the 3D screen, click View Solution." />
+            <img src={viewSolution} alt={viewSolution} style={{
+                display: "flex",
+                alignItems: "center",
+                alignSelf: "center",
+                height: 55,
+                width: 150}}/>
         </div>
     );
 };
@@ -56,7 +78,7 @@ const ShowInfo = () => {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center'
-            }}/>
+            }}/><br/>
             {help}
         </div>
     )
