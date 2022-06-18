271ce4b24b9bf868457054c2e3d53dc88dc712a2
Yarin <dado.yarin@gmail.com>
2022-06-18 22:41:22 +0300

    fixed alignment issues (where hebrew gets a different code)

--- src/components/CustomText.js
+++ src/components/CustomText.js
@@ -1,10 +1,12 @@
 import { Typography } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 
+const getAlignment = (language) => language.includes('he') ? 'right' : 'left';
+
 const Text = (props) => {
     const { text } = props;
     const { t, i18n } = useTranslation();
-    const alignment = (i18n.language === 'he') ? 'right' : 'left';
+    const alignment = getAlignment(i18n.language);
     return (
         <Typography {...props} align={alignment}>{t(text)}</Typography>
     );
